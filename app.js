7fd426211f294d0aee43451a6daa7e91534fc8b1
Pierre Karpov <push.default>
2016-11-04 18:16:11 -0700

    mongodb stuff added

--- app.js
+++ app.js
@@ -42,6 +42,74 @@ app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("We re in baby");
+
+  /*
+  .d8888.  .o88b. db   db d88888b .88b  d88.  .d8b.
+  88'  YP d8P  Y8 88   88 88'     88'YbdP`88 d8' `8b
+  `8bo.   8P      88ooo88 88ooooo 88  88  88 88ooo88
+    `Y8b. 8b      88~~~88 88~~~~~ 88  88  88 88~~~88
+  db   8D Y8b  d8 88   88 88.     88  88  88 88   88
+  `8888Y'  `Y88P' YP   YP Y88888P YP  YP  YP YP   YP
+  */
+  var kittySchema = mongoose.Schema({
+    name: String
+  });
+  kittySchema.methods.speak = function () {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  }
+
+  /*
+  .88b  d88.  .d88b.  d8888b. d88888b db
+  88'YbdP`88 .8P  Y8. 88  `8D 88'     88
+  88  88  88 88    88 88   88 88ooooo 88
+  88  88  88 88    88 88   88 88~~~~~ 88
+  88  88  88 `8b  d8' 88  .8D 88.     88booo.
+  YP  YP  YP  `Y88P'  Y8888D' Y88888P Y88888P
+  */
+
+  var Kitten = mongoose.model('Kitten', kittySchema);
+
+
+  /*
+  d8888b.  .d8b.  d888888b  .d8b.        .d88b.  d8888b.
+  88  `8D d8' `8b `~~88~~' d8' `8b      .8P  Y8. 88  `8D
+  88   88 88ooo88    88    88ooo88      88    88 88oodD'
+  88   88 88~~~88    88    88~~~88      88    88 88~~~
+  88  .8D 88   88    88    88   88      `8b  d8' 88
+  Y8888D' YP   YP    YP    YP   YP       `Y88P'  88
+  */
+  var silence = new Kitten({ name: 'Silence' });
+  //console.log(silence.name);
+  //var fluffy = new Kitten({ name: 'fluffy' });
+
+  /*
+  silence.save(function (err, silence) {
+    if (err) return console.error(err);
+    silence.speak();
+  });*/
+
+  Kitten.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  });
+  Kitten.find({ name: /^fluff/ }, function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  });
+
+}); // end of db.once
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
