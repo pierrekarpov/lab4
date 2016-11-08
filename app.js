8676f36e6533412f7dd81baf26c47c310ac56889
Pierre Karpov <push.default>
2016-11-08 13:19:45 -0800

    jahsjhg

--- app.js
+++ app.js
@@ -98,16 +98,32 @@ db.once('open', function() {
     silence.speak();
   });*/
 
-  Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-  });
+  // Kitten.find(function (err, kittens) {
+  //   if (err) return console.error(err);
+  //   console.log(kittens);
+  // });
   Kitten.find({ name: /^fluff/ }, function (err, kittens) {
     if (err) return console.error(err);
+    console.log("In app.js");
+    console.log("/^fluff/ objects:");
     console.log(kittens);
   });
 
+  app.locals.Kitten = Kitten;
 }); // end of db.once
+
+
+/*
+.d8888.  .d8b.  db    db d88888b      d8888b. d8888b.      d8888b. d88888b d88888b
+88'  YP d8' `8b 88    88 88'          88  `8D 88  `8D      88  `8D 88'     88'
+`8bo.   88ooo88 Y8    8P 88ooooo      88   88 88oooY'      88oobY' 88ooooo 88ooo
+  `Y8b. 88~~~88 `8b  d8' 88~~~~~      88   88 88~~~b.      88`8b   88~~~~~ 88~~~
+db   8D 88   88  `8bd8'  88.          88  .8D 88   8D      88 `88. 88.     88
+`8888Y' YP   YP    YP    Y88888P      Y8888D' Y8888P'      88   YD Y88888P YP
+*/
+
+app.locals.mongoose = mongoose;
+app.locals.db = db;
 
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/project.js
+++ routes/project.js
@@ -5,4 +5,31 @@ exports.viewProject = function(req, res) {
   res.render('project', {
     'projectName': name
   });
+
+  testDB(req);
+};
+
+function testDB(req) {
+  // var mongoose = req.app.locals.mongoose;
+  // mongoose.connect('mongodb://localhost/test');
+  // var db = req.app.locals.db;
+
+  var Kitten = req.app.locals.Kitten;
+
+
+
+  // var projectCat2 = new Kitten({ name: 'Project:cat2' });
+  // projectCat2.save(function (err, silence) {
+  //   if (err) return console.error(err);
+  //   Kitten.find(function (err, kittens) {
+  //     if (err) return console.error(err);
+  //     console.log(kittens);
+  //   });
+  // });
+
+  Kitten.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log("In project.js");
+    console.log(kittens);
+  });
 };
